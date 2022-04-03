534fa4e3f2706856a0818c1d54588f8b638bfb93
Mohamed Ashraf <mos@corelia.ai>
2022-04-03 14:42:11 +0200

    servic1

--- UI/src/app/services/Transaction/transaction.service.ts
+++ UI/src/app/services/Transaction/transaction.service.ts
@@ -12,8 +12,20 @@ export class TransactionService {
   addTransaction(data:any)
   {
     return this.http.post(
-      `${environment.apiUrl}Transactions`,data
+      `${environment.apiUrl}en/Transactions`,data
       // ,  { responseType: 'text' }
     );
+  }
+  getallholiday()
+  {
+    return this.http.get(`${environment.apiUrl}en/leaves/1/2022`);
+  }
+  getallerrands()
+  {
+    return this.http.get(`${environment.apiUrl}en/leaves/2/2022`);
+  }
+  getallpermissions()
+  {
+    return this.http.get(`${environment.apiUrl}en/leaves/3/2022`);
   }
 }
