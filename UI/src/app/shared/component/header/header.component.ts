2d1c5fffee1aaf9715ea8cf1efd3546d91897cb2
Mohamed Ashraf <mos@corelia.ai>
2022-04-26 02:15:18 +0200

    4/26/2022(2:15)

--- UI/src/app/shared/component/header/header.component.ts
+++ UI/src/app/shared/component/header/header.component.ts
@@ -1,11 +1,13 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { LoginService } from 'src/app/services/login/login.service';
+import { Settings } from '../../app.sttings.model';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  public settings: Settings;
   name :string;
   user :any;
   Username: any;
@@ -17,6 +19,11 @@ export class HeaderComponent implements OnInit {
   ngOnInit(): void {
     this.name= JSON.parse(localStorage.getItem('username') as any);
   }
+  public changeTheme(theme:any){
+    this.settings.theme.skin = theme;
+    sessionStorage["skin"] = theme;        
+}
+
   toggleSidebar() {
     this.toggleSidebarForMe.emit();
   }
